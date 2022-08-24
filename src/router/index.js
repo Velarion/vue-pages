57514d8a6143424ea89a9be2c0756b04e295527b
Velarion <omar.afgh@hotmail.com>
2022-08-24 21:13:08 +0200

    Resolved issue with the router.

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
@@ -17,8 +17,8 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
-  routes
-})
+  history: createWebHistory(),
+  routes,
+});
 
 export default router
